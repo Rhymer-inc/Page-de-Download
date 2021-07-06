3d9bb278c39b3c40055630c5c38ebe734e23663d
Davy Pereira <davypereira1802@gmail.com>
2021-07-06 18:35:33 -0300

    version 1.0

---
+++ js/animacoes.js
@@ -0,0 +1,8 @@
+function aparecer(){
+    var div = document.getElementById('menu')
+    div.innerHTML = ''
+}
+function sumir(){
+    var div = document.getElementById('menu')
+    div.innerHTML = ''
+}
